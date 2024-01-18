aa6076e90a6ff1415c816b02b68fece2041828c3
Kris Ivan <krisztian.ivan@gmail.com>
2024-01-18 17:34:54 +0000

    added settings to enable returning of static files and added index.html static webpage

--- PracticalApps/Northwind.Web/Startup.cs
+++ PracticalApps/Northwind.Web/Startup.cs
@@ -18,9 +18,13 @@ public class Startup
 
         app.UseHttpsRedirection();
 
+        app.UseDefaultFiles(); // index.html, default.html, and so on
+
+        app.UseStaticFiles();
+
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapGet("/", () => "Hello World!");
+            endpoints.MapGet("/hello", () => "Hello World!");
         });
     }
 }
